f174d567f1ea65856cd3695d44f97c557f2a71e3
Martin Paul Eve <martin@martineve.com>
2021-03-12 11:53:29 +0000

    Add timeset

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import re
 import requests
 import lxml
 import bs4
+from datetime import datetime
+from dateutil import parser
 
 
 def scrape():
@@ -16,7 +18,7 @@ def scrape():
     just_reviewed = []
 
     edges = []
-    for y in range(22, 44):
+    for y in range(1, 44):
         for x in range(1, 24):
             print ("v{0}n{1}".format(y, x))
             content = requests.get('https://www.lrb.co.uk/the-paper/'
@@ -24,46 +26,56 @@ def scrape():
 
             soup = bs4.BeautifulSoup(content)
 
-            ToC = soup.find_all("div", {"class": "toc-grid-items"})[0]
+            try:
+                header = soup.find_all("h1", {"class": "toc-title"})[0]
 
-            ToCs = ToC.find_all("a")
+                date = header.getText().strip().split('·')[1].strip()
+                date = parser.parse(date)
 
-            for ToCitem in ToCs:
+                timeset = date.isoformat()
 
-                author = ToCitem.find_all("h3")[0]
-                author = author.getText().strip()
-                by = ToCitem.find_all("span", {"class": "by"})
-                if len(by) > 0:
+                ToC = soup.find_all("div", {"class": "toc-grid-items"})[0]
 
-                    for item in by:
-                        regex = r"by\s+(.+?)\.<"
-                        title_search = re.search(regex, str(item),
-                                                 re.IGNORECASE)
+                ToCs = ToC.find_all("a")
 
-                        if title_search:
-                            byline = title_search.group(1)
+                for ToCitem in ToCs:
 
-                            # construct the node table
-                            if author not in authors:
-                                author_index += 1
-                                authors[author] = author_index
-                                if author not in just_reviewers:
-                                    just_reviewers.append(author)
+                    author = ToCitem.find_all("h3")[0]
+                    author = author.getText().strip()
+                    by = ToCitem.find_all("span", {"class": "by"})
+                    if len(by) > 0:
 
-                            byline = byline.strip()
+                        for item in by:
+                            regex = r"by\s+(.+?)\.<"
+                            title_search = re.search(regex, str(item),
+                                                     re.IGNORECASE)
 
-                            if byline not in authors:
-                                author_index += 1
-                                authors[byline] = author_index
-                                if byline not in just_reviewed:
-                                    just_reviewed.append(byline)
+                            if title_search:
+                                byline = title_search.group(1)
 
-                            edges.append([authors[author], authors[byline],
-                                          y, x])
+                                # construct the node table
+                                if author not in authors:
+                                    author_index += 1
+                                    authors[author] = author_index
+                                    if author not in just_reviewers:
+                                        just_reviewers.append(author)
+
+                                byline = byline.strip()
+
+                                if byline not in authors:
+                                    author_index += 1
+                                    authors[byline] = author_index
+                                    if byline not in just_reviewed:
+                                        just_reviewed.append(byline)
+
+                                edges.append([authors[author], authors[byline],
+                                              y, x, timeset])
+            except:
+                print('Error getting ToC for v{0}i{1}'.format(y, x))
 
     with open('/home/martin/LRB_edges.csv', 'w', newline='') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-        wr.writerow(['source', 'target', 'vol', 'no'])
+        wr.writerow(['source', 'target', 'vol', 'no', 'timeset'])
         for row in edges:
             wr.writerow(row)
 
@@ -74,7 +86,8 @@ def scrape():
             wr.writerow([authors[row], row])
 
 
-def find_circular(authors, edges, input_chain, reviewer_to_check, lowest_edge):
+def find_circular(authors, edges, input_chain, reviewer_to_check, lowest_edge=0,
+                  end_search=0):
     # a function that finds circularities of reviews
 
     if reviewer_to_check in input_chain:
@@ -92,8 +105,10 @@ def find_circular(authors, edges, input_chain, reviewer_to_check, lowest_edge):
         # edges CSV file
         if edge[0] == reviewer_to_check: # and int(edge[4]) > lowest_edge:
             new_input_chain = input_chain
+            #find_circular(authors, edges, new_input_chain + [reviewer_to_check],
+            #              edge[1], int(edge[4]))
             find_circular(authors, edges, new_input_chain + [reviewer_to_check],
-                          edge[1], int(edge[4]))
+                          edge[1])
 
 
 def translate(input_list):
@@ -139,6 +154,93 @@ def translate(input_list):
                     break
 
 
+def top10():
+    edges = []
+    authors = {}
+
+    reviewers = []
+    reviewed = []
+
+    reviewer_count = {}
+
+    with open('/home/martin/LRB_edges.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+
+        for row in reader:
+            edges.append(row)
+            if row[0] not in reviewers:
+                reviewers.append(row[0])
+
+            if row[0] not in reviewer_count.keys():
+                reviewer_count[row[0]] = 1
+            else:
+                reviewer_count[row[0]] = reviewer_count[row[0]] + 1
+
+            if row[1] not in reviewed:
+                reviewed.append(row[1])
+
+    sorted_reviews = sorted(reviewer_count.items(), key=lambda item: item[1], reverse=True)
+    print(sorted_reviews)
+
+    top = []
+    x = 0
+
+    for k, v in sorted_reviews:
+        if x > 10:
+            break
+
+        top.append(k)
+        x = x + 1
+
+    with open('/home/martin/LRB_nodes.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+
+        for row in reader:
+            authors[row[0]] = row[1]
+
+    for item in top:
+        print(authors[item])
+
+    return
+
+def detect(reviewer, reviewee):
+    edges = []
+    authors = {}
+
+    reviewers = []
+    reviewed = []
+
+    with open('/home/martin/LRB_edges.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+
+        for row in reader:
+            edges.append(row)
+            if row[0] not in reviewers:
+                reviewers.append(row[0])
+
+            if row[1] not in reviewed:
+                reviewed.append(row[1])
+
+    reviewer_id = 0
+    reviewee_id = 0
+
+    with open('/home/martin/LRB_nodes.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+
+        for row in reader:
+            authors[row[0]] = row[1]
+
+            if row[1] == reviewer:
+                reviewer_id = row[0]
+
+            if row[1] == reviewee:
+                reviewee_id = row[0]
+
+    print("Finding")
+    find_circular(authors, edges, [], reviewer_id, end_search=reviewee_id)
+
+    return
+
 def scan():
 
     edges = []
@@ -171,8 +273,7 @@ def scan():
 
 
 if __name__ == '__main__':
-    # uncomment the function that you want to execute
-    # don't forget to change the paths from /home/martin throughout
-    # scan()
-    # scrape()
+    scrape()
+    #top10()
+    #detect('Bernard Porter', 'Miles Taylor')
     pass
